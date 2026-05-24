af0aaa7ecf886361491990f7b90aefdd1e7dac9f
ckengus <ckengus@gmail.com>
2026-05-25 00:51:14 +0900

    Add project skeleton

---
+++ Assets/AnimaSpire/Scripts/Combat/CombatManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("CombatManager initialized.");
+    }
+}
---
+++ Assets/AnimaSpire/Scripts/Stage/StageManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("StageManager initialized.");
+    }
+}
